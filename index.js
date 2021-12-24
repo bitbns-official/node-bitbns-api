a639dff142ceaaa931d79d6810aecccfdd8109ba
its-mohnish <84853776+its-mohnish@users.noreply.github.com>
2021-12-24 15:04:43 +0400

    Update index.js

--- index.js
+++ index.js
@@ -584,7 +584,37 @@ class bitbnsApi{
 
 // Margin Trading starts here - V2
 
-placeMarginOrders(orders_obj, callback) {
+  placeMarginOrders(orders_obj, callback) {
+      this.requestAuthenticate2(orders_obj, callback);
+          if(this.verifyApiKeys(this.apiKeys)){
+            let body = orders_obj;
+            this.makePostRequest2("marginOrders", body, callback);
+          }else{
+            return callback("apiKeys Not Found , Please intialize it first","");
+      }
+  }
+
+  cancelMarginOrder(orders_obj, callback) {
+      this.requestAuthenticate2(orders_obj, callback);
+          if(this.verifyApiKeys(this.apiKeys)){
+            let body = orders_obj;
+            this.makePostRequest2("marginOrders", body, callback);
+          }else{
+            return callback("apiKeys Not Found , Please intialize it first","");
+      }
+  }
+
+  settleMarginPartial(orders_obj, callback) {
+      this.requestAuthenticate2(orders_obj, callback);
+          if(this.verifyApiKeys(this.apiKeys)){
+            let body = orders_obj;
+            this.makePostRequest2("marginOrders", body, callback);
+          }else{
+            return callback("apiKeys Not Found , Please intialize it first","");
+      }
+  }
+
+  settleMargin(orders_obj, callback) {
     this.requestAuthenticate2(orders_obj, callback);
         if(this.verifyApiKeys(this.apiKeys)){
           let body = orders_obj;
@@ -592,163 +622,156 @@ placeMarginOrders(orders_obj, callback) {
         }else{
           return callback("apiKeys Not Found , Please intialize it first","");
     }
-}
+  }
 
-cancelMarginOrder(orders_obj, callback) {
-    this.requestAuthenticate2(orders_obj, callback);
-        if(this.verifyApiKeys(this.apiKeys)){
-          let body = orders_obj;
-          this.makePostRequest2("marginOrders", body, callback);
-        }else{
-          return callback("apiKeys Not Found , Please intialize it first","");
+  listMarginExecuted(orders_obj, callback) {
+      this.requestAuthenticate2(orders_obj, callback);
+          if(this.verifyApiKeys(this.apiKeys)){
+            let body = orders_obj;
+            this.makePostRequest2("marginOrders", body, callback);
+          }else{
+            return callback("apiKeys Not Found , Please intialize it first","");
+      }
+  }
+
+
+  listMarginPending(orders_obj, callback) {
+      this.requestAuthenticate2(orders_obj, callback);
+          if(this.verifyApiKeys(this.apiKeys)){
+            let body = orders_obj;
+            this.makePostRequest2("marginOrders", body, callback);
+          }else{
+            return callback("apiKeys Not Found , Please intialize it first","");
+      }
+  }
+
+
+  listMarginMarketOrders(orders_obj, callback) {
+      this.requestAuthenticate2(orders_obj, callback);
+          if(this.verifyApiKeys(this.apiKeys)){
+            let body = orders_obj;
+            this.makePostRequest2("marginOrders", body, callback);
+          }else{
+            return callback("apiKeys Not Found , Please intialize it first","");
+      }
+  }
+
+// Public endpoints starts here
+	
+  fetchTickers(callback){
+    const options = {
+      url: this.baseURL3 + '/order/getTickerWithVolume',
+      method: 'GET'
     }
-}
 
-settleMarginPartial(orders_obj, callback) {
-    this.requestAuthenticate2(orders_obj, callback);
-        if(this.verifyApiKeys(this.apiKeys)){
-          let body = orders_obj;
-          this.makePostRequest2("marginOrders", body, callback);
-        }else{
-          return callback("apiKeys Not Found , Please intialize it first","");
-    }
-}
-
-settleMargin(orders_obj, callback) {
-  this.requestAuthenticate2(orders_obj, callback);
-      if(this.verifyApiKeys(this.apiKeys)){
-        let body = orders_obj;
-        this.makePostRequest2("marginOrders", body, callback);
+    request(options, function(error, res, body) {
+      if(!error){
+        try{
+          return callback("", JSON.parse(body));
+        }catch(err){
+          return callback("Parsing error : fetchTickers", "");
+        }
       }else{
-        return callback("apiKeys Not Found , Please intialize it first","");
-  }
-}
-
-listMarginExecuted(orders_obj, callback) {
-    this.requestAuthenticate2(orders_obj, callback);
-        if(this.verifyApiKeys(this.apiKeys)){
-          let body = orders_obj;
-          this.makePostRequest2("marginOrders", body, callback);
-        }else{
-          return callback("apiKeys Not Found , Please intialize it first","");
-    }
-}
-
-
-listMarginPending(orders_obj, callback) {
-    this.requestAuthenticate2(orders_obj, callback);
-        if(this.verifyApiKeys(this.apiKeys)){
-          let body = orders_obj;
-          this.makePostRequest2("marginOrders", body, callback);
-        }else{
-          return callback("apiKeys Not Found , Please intialize it first","");
-    }
-}
-
-
-listMarginMarketOrders(orders_obj, callback) {
-    this.requestAuthenticate2(orders_obj, callback);
-        if(this.verifyApiKeys(this.apiKeys)){
-          let body = orders_obj;
-          this.makePostRequest2("marginOrders", body, callback);
-        }else{
-          return callback("apiKeys Not Found , Please intialize it first","");
-    }
-}
-
-fetchTickers(callback){
-  const options = {
-    url: this.baseURL3 + '/order/getTickerWithVolume',
-    method: 'GET'
+        return callback("Error in fetching tickers");
+      }
+    });
   }
 
-  request(options, function(error, res, body) {
-    if(!error){
-      try{
-        return callback("", JSON.parse(body));
-      }catch(err){
-        return callback("Parsing error : fetchTickers", "");
+  fetchOrderBook(coinName, marketName, depth, callback){
+    const options = {
+      url: this.baseURL3 + '/exchangeData/orderBook',
+      method: 'GET',
+      qs: {
+        market: marketName,
+        coin: coinName
       }
-    }else{
-      return callback("Error in fetching tickers");
-    }
-  });
-}
+    };
 
-fetchOrderBook(coinName, marketName, depth, callback){
-  const options = {
-    url: this.baseURL3 + '/exchangeData/orderBook',
-    method: 'GET',
-    qs: {
-      market: marketName,
-      coin: coinName
-    }
-  };
-
-  request(options, function(error, res, body){
-    if(!error){
-      try{
-        body = JSON.parse(body);
-        body['asks'].splice(depth);
-        body['bids'].splice(depth);
-        return callback("", {data: body, error: null, status: 1});
-      }catch(err){
-        return callback("Parsing error : fetchOrderBook", "");
+    request(options, function(error, res, body){
+      if(!error){
+        try{
+          body = JSON.parse(body);
+          body['asks'].splice(depth);
+          body['bids'].splice(depth);
+          return callback("", {data: body, error: null, status: 1});
+        }catch(err){
+          return callback("Parsing error : fetchOrderBook", "");
+        }
+      }else{
+        return callback("Error in fetching order book", "");
       }
-    }else{
-      return callback("Error in fetching order book", "");
-    }
-  });
-}
+    });
+  }
 
-fetchTrades(coinName, marketName, limit, callback){
-  const options = {
-    url: this.baseURL3 + '/exchangeData/tradedetails',
-    method: 'GET',
-    qs: {
-      market: marketName,
-      coin: coinName
-    }
-  };
-
-  request(options, function(error, res, body){
-    if(!error){
-      try{
-        body = JSON.parse(body).reverse();
-        return callback("",{data: body.splice(0, limit), error: null, status: 1});
-      }catch(err){
-        return callback("Parsing error : fetchTrades", "");
+  fetchTrades(coinName, marketName, limit, callback){
+    const options = {
+      url: this.baseURL3 + '/exchangeData/tradedetails',
+      method: 'GET',
+      qs: {
+        market: marketName,
+        coin: coinName
       }
-    }else{
-      return callback("Error in fetching trade details", "");
-    }
-  });
+    };  
 
-}
-
-fetchOhlcv(coinName, marketName, page, callback){
-  const options = {
-    url: this.baseURL3 + '/exchangeData/ohlc',
-    method: 'GET',
-    qs: {
-      coin: `${coinName}_${marketName}`,
-      page: page
-    }
-  };
-
-  request(options, function(error, res, body){
-    if(!error){
-      try{
-        body = JSON.parse(body);
-        return callback("", {data: body[0]['data'], error: null, status: 1});
-      }catch(err){
-        return callback("Parsing error : fetchOhlcv", "");
+    request(options, function(error, res, body){
+      if(!error){
+        try{
+          body = JSON.parse(body).reverse();
+          return callback("",{data: body.splice(0, limit), error: null, status: 1});
+        }catch(err){
+          return callback("Parsing error : fetchTrades", "");
+        }
+      }else{
+        return callback("Error in fetching trade details", "");
       }
-    }else{
-      return callback("Error in fetching ohlcv data", "");
-    }
-  });
-}
+    });
+
+  }
+
+  fetchMarkets(callback){
+    const options = {
+      url: this.baseURL3 + '/order/fetchMarkets/',
+      method: 'GET',
+    };  
+
+    request(options, function(error, res, body){
+      if(!error){
+        try{
+          body = JSON.parse(body);
+          return callback("",{data: body, error: null, status: 1});
+        }catch(err){
+          return callback("Parsing error : fetchMarkets", "");
+        }
+      }else{
+        return callback("Error in fetching trade details", "");
+      }
+    });
+
+  }
+
+  fetchOhlcv(coinName, marketName, page, callback){
+    const options = {
+      url: this.baseURL3 + '/exchangeData/ohlc',
+      method: 'GET',
+      qs: {
+        coin: `${coinName}_${marketName}`,
+        page: page
+      }
+    };
+
+    request(options, function(error, res, body){
+      if(!error){
+        try{
+          body = JSON.parse(body);
+          return callback("", {data: body[0]['data'], error: null, status: 1});
+        }catch(err){
+          return callback("Parsing error : fetchOhlcv", "");
+        }
+      }else{
+        return callback("Error in fetching ohlcv data", "");
+      }
+    });
+  }
 
 
 // Partner APIS start here - These apis need pre approval from Bitbns team
