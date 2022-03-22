2f5f36e4a23758967e331503918d2dc428a9c567
Prashant Singh <atiprashant@gmail.com>
2022-03-22 18:29:05 +0530

    Update payment-whook.js

--- payment-whook.js
+++ payment-whook.js
@@ -7,10 +7,11 @@ STEPS TO FOLLOW FOR USING BITBNS CRYPTO PAYMENT WEBHOOK
 3. Following fields will be there in the webhook payload - description, amount, upi_id, merchant_id, reference_number, user_identifier, signature, ack_type 
 5. Verify signature by calculating it yourself with secret shared in step 1
 
-let stringToEncode = "description=" + (description) + "&upi_id=" + upi_id + "&amount=" + amount + "&merchant_id=" + merchant_id + "&reference_number=" + reference_number + "&user_identifier=" + user_identifier + "&ack_type=" + ack_type;
+let stringToEncode = "description=" + (description) + "&upi_id=" + upi_id + "&source=" + source + "&bitbns_ref=" + ref_no_bitbns + "&amount=" + amount + "&merchant_id=" + merchant_id + "&reference_number=" + reference_number + "&user_identifier=" + user_identifier + "&ack_type=" + ack_type;
 const calculatedSignature = crypto.createHmac('sha256', apiSecret).update(stringToEncode).digest('hex');
 
 POSSIBLE VALUES OF ack_type {PAYMENT_REC, PAYMENT_MAPPED}
+source - last 2 digits of bank account
 
 6. You can accept the payment/credit in case calculatedSignature is matching to signature passed in the payload
 
