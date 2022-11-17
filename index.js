b77b78557f8a7692881c57430fc1fcd0c4e7de57
Swapnil K <78466076+swapnil-buyhatke@users.noreply.github.com>
2022-11-17 12:36:28 +0530

    feat: added listOpenOrder, placeOrder, cancelOrder

--- index.js
+++ index.js
@@ -1225,6 +1225,41 @@ fetchMyHistory(dataObj, callback){
     }
   }
 
+
+  listOpenOrdersOther(symbol, market, callback) {
+    this.requestAuthenticate(symbol, callback);
+
+    if (this.verifyApiKeys(this.apiKeys)) {
+      let body = { page: 0, market };
+      this.makePostRequest(symbol, "listOpenOrdersOther", body, callback);
+    } else {
+      return callback("apiKeys Not Found , Please intialize it first", "");
+    }
+  }
+
+
+  placeOrderOther(symbol, quantity, rate, orderType, market, callback) {
+    if(orderType != "buy" && orderType != "sell")
+      return callback("Order type should be buy or sell", "");
+    this.requestAuthenticate(symbol, callback);
+    if (this.verifyApiKeys(this.apiKeys)) {
+      let body = { quantity, rate, market };
+      this.makePostRequest(symbol, orderType == "buy" ? "placeBuyOrderOther": "placeSellOrderOther", body, callback);
+    } else {
+      return callback("apiKeys Not Found , Please intialize it first", "");
+    }
+  }
+  
+  cancelOrderOther(symbol, entry_id, market, callback) {
+    this.requestAuthenticate(symbol, callback);
+    if (this.verifyApiKeys(this.apiKeys)) {
+      let body = { entry_id, market };
+      this.makePostRequest(symbol, "cancelOrderOther", body, callback);
+    } else {
+      return callback("apiKeys Not Found , Please intialize it first", "");
+    }
+  }
+
 }
 
 module.exports = bitbnsApi;
